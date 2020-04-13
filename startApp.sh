41e469631b77d3b271287c7f4739a98c17591c5a
Nathan Cross <nmcross@uw.edu>
2020-04-13 14:48:22 -0700

    uses requirements file and doesn't rebuild conda env unless not present

--- startApp.sh
+++ startApp.sh
@@ -6,15 +6,15 @@ ENVS=$(conda env list | awk '{print $1}' )
 flag=$false
 for env in $ENVS; 
 do 
-	if [ "$env" == "TEST" ]; then 
+	if [ "$env" == "DicomAnnotatorEnv" ]; then 
 		flag=$true; 
 	fi;
 done
-if $flag ; then
+if ! $flag ; then
 	conda config --append channels anaconda
 	conda config --append channels conda-forge
 	conda config --append channels simpleitk
-	conda create -n DicomAnnotatorEnv python=3.7
+	conda create -n DicomAnnotatorEnv python=3.7 --file ./requirements.txt
 fi
 conda activate DicomAnnotatorEnv
 export PYTHONPATH=$PYTHONPATH:$PWD
